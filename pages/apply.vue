7cd8e7c7b205c44a02be2bb402ae0a9e0f4f4559
clpowe <clpowe@gmail.com>
2023-12-08 22:37:51 -0500

    updated homepage

--- pages/apply.vue
+++ pages/apply.vue
@@ -2,12 +2,58 @@
 	import { createZodPlugin } from '@formkit/zod'
 	import { z } from 'zod'
 
+	useSeoMeta({
+		title: 'Agape Christian Application',
+		description: '[description]',
+		ogTitle: '[og:title]',
+		ogDescription: '[og:description]',
+		ogImage: '[og:image]',
+		ogUrl: '[og:url]',
+		twitterTitle: '[twitter:title]',
+		twitterDescription: '[twitter:description]',
+		twitterImage: '[twitter:image]',
+		twitterCard: 'summary'
+	})
+
+	useHead({
+		htmlAttrs: {
+			lang: 'en'
+		},
+		link: [
+			{
+				rel: 'icon',
+				type: 'image/png',
+				href: '/favicon.png'
+			}
+		]
+	})
+
 	const supabase = useSupabaseClient()
 	const files = ref([])
 	import { reset } from '@formkit/core'
 
+	const communication = [
+		{ label: 'Email', value: 'Email' },
+		{ label: 'Phone', value: 'Phone' },
+		{ label: 'Text', value: 'Text' }
+	]
+
+	const maritalStatus = [
+		{ label: 'Married', value: 'Married' },
+		{ label: 'Single', value: 'Single' },
+		{ label: 'Widowed', value: 'Widowed' }
+	]
+
+	const tshirt = [
+		{ label: 'xs', value: 'xs' },
+		{ label: 'sm', value: 's' },
+		{ label: 'md', value: 'md' },
+		{ label: 'lg', value: 'lg' },
+		{ label: 'xl', value: 'xl' },
+		{ label: 'xxl', value: 'xxl' }
+	]
+
 	async function handleSubmit(formData) {
-		// console.log(formData.ms.EducationHistory.history)
 		await $fetch('/api/apply', {
 			method: 'POST',
 			body: formData
@@ -41,11 +87,84 @@
 			@submit="handleSubmit"
 			id="Application"
 		>
-			<FormKit type="multi-step" name="ms" tab-style="progress">
+			<FormKit type="multi-step" name="ms" tab-style="tab">
 				<FormKit type="step" name="PersonalInformation">
 					<FormKit type="text" label="Full Name" validation="required" />
+					<FormKit type="text" label="Mailing Address" validation="required" />
+					<FormKit
+						type="email"
+						label="Email Address"
+						validation="required|email"
+					/>
+					<FormKit
+						type="tel"
+						label="Phone Number"
+						placeholder="xxx-xxx-xxxx"
+						validation="matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+						:validation-messages="{
+							matches: 'Phone number must be in the format xxx-xxx-xxxx'
+						}"
+						validation-visibility="dirty"
+					/>
+					<FormKit
+						type="dropdown"
+						label="Preferred Method of Communication"
+						placeholder="Email"
+						:options="communication"
+					/>
+					<FormKit
+						type="dropdown"
+						label="Marital Status"
+						placeholder="Single"
+						:options="maritalStatus"
+						validation="required"
+						:validation-messages="{
+							matches:
+								'This information is required to help us personalize the services we provide.'
+						}"
+						validation-visibility="dirty"
+					/>
+					<FormKit
+						type="textarea"
+						label="How many children do you have and what are their ages?"
+						validation="required"
+						validation-visibility="dirty"
+						:validation-messages="{
+							required:
+								'This information is required to help us personalize the services we provide.'
+						}"
+					/>
+					<FormKit
+						type="textarea"
+						label="Do you have any other obligations?"
+						validation="required"
+						help="Do you have obligations that could potentially take away from your study time such as caring for a sick family member, planned trips, etc.?  If so, please explain."
+						validation-visibility="dirty"
+						:validation-messages="{
+							required:
+								'This information is required to help us personalize the services we provide.'
+						}"
+					/>
+					<FormKit
+						type="dropdown"
+						label="What is your T-Shirt Size"
+						placeholder="MD - Medium"
+						:options="tshirt"
+					/>
 				</FormKit>
+
 				<FormKit type="step" name="EducationHistory">
+					<FormKit
+						type="text"
+						label="What law school did you attend or what law school are you attending? "
+						placeholder="University of South Florica"
+					/>
+					<FormKit
+						type="text"
+						label="What what year did you graduate or will you graduate?"
+						placeholder="2024"
+					/>
+
 					<FormKit type="file" name="history" multiple="true"></FormKit>
 					<template #stepNext>
 						<FormKit type="submit" />
--- pages/index.vue
+++ pages/index.vue
@@ -28,13 +28,42 @@
 		]
 	})
 
-	const { data } = await useAsyncData('home', () => queryContent('/').findOne())
+	//const { data } = await useAsyncData('home', () => queryContent('/').findOne())
 </script>
 
 <template>
-	<ContentRenderer :value="data">
+	<!-- <ContentRenderer :value="data">
 		<main>
 			<ContentRendererMarkdown :value="data" class="content-grid flow" />
 		</main>
-	</ContentRenderer>
+	</ContentRenderer> -->
+
+	<h1 class="editable">We help <span>you</span> pass the bar</h1>
+	<p>Personalized service with proven results</p>
+
+	<img
+		src="https://ik.imagekit.io/cpds/Agape_Christian/HeroImageMd_7IJG72IBh.png?updatedAt=1688698892851"
+		alt="Young man with glasses
+		studying"
+	/>
+	<h2 class="editable">Our <span> service </span></h2>
+	<p>
+		Agape Christian Bar Prep offers many services to help students prepare for
+		success in law school and on the bar exam.  Our services are customized to
+		clearly identify and meet the individual needs of law students and law
+		school graduates.
+	</p>
+
+	<h2 class="editable">
+		How our Program is
+		<span> diffrent</span>
+	</h2>
+	<p class="editable">
+		Agape Christian Bar Prep helps students prepare to pass the bar exam through
+		personalized services that offer a holistic approach to bar exam
+		preparation. Our program is uniquely designed to meet the individual
+		spiritual, mental, emotional and academic needs of students while
+		simultaneously empowering them to conquer the bar exam and achieve their
+		dreams of becoming attorneys.
+	</p>
 </template>
