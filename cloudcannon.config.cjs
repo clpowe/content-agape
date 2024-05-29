a1a9b197324955f80e9f9b05e61dc75269fbe50b
Christopher Lamar Powe <33496540+clpowe@users.noreply.github.com>
2024-05-28 20:48:37 -0400

    Update and rename cloudcannon.config.cjs to cloudcannon.config.yml

--- cloudcannon.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
- 
-  // Read from ./src instead of .
-  source: 'src',
-
-  // Write to ./output/_cloudcannon/info.json instead of ./_cloudcannon/info.json
-  output: 'output',
-    
-};
